01fe0d9bad1ed2a7eae2a3dacda5ce6abb9a0328
shawnsnyk <38260497+shawnsnyk@users.noreply.github.com>
2024-10-01 15:24:16 -0400

    Update and rename log2.py to log3.py

---
+++ logs/log3.py
@@ -0,0 +1,5 @@
+sqlstr="postgresql://log_service:U34Nx69343g!h93w@cloud.google.com:5132/log"
+txtPwd="myPass9134!"
+aws="2DpTkBeYtb0voaW/yFN9ipRM8xPY9vVUiXS1d5N+!moregibberishsirguhorwg"
+
+print("logfiletester")
