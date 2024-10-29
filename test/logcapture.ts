9bd1f63927a3ee555fe146e0870b493b610979d6
shawnsnyk <38260497+shawnsnyk@users.noreply.github.com>
2024-10-29 14:22:37 -0400

    Create logcapture.ts

---
+++ test/logcapture.ts
@@ -0,0 +1,9 @@
+LOG: 2024-11-01 00:03 Failed to send card to processor
+LOG: 2024-11-01 00:02 critical failure - postgresql://booking_services:U73Mx673g!h93w@cloud.google.com:5431/bookings
+token Capture: my238456est234324Pass1!
+token Capture: myTes34565743tPass2!
+LOG: 2024-10-22 00:02 critical failure - postgresql://booking_services:U73Mx673g!h93w@cloud.google.com:3431/bookings
+LOG: 2024-10-28 00:03 Failed to send card to processor
+token Capture: myPass2345747432434234242!
+LOG: 2024-10-07 03:02 critical failure - postgresql://booking_services:U73Mx673g!h93w@cloud.google.com:3131/capture
+token Capture: myPass2327474324!
